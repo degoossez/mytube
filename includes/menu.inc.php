06c3d6f4c4f97f8c42714226c7cd80d4f68ec910
root <degoossez>
2014-01-09 12:38:21 -0800

    added bootstrap

--- includes/menu.inc.php
+++ includes/menu.inc.php
@@ -1,6 +1,27 @@
-<nav>
-    <ul id="menu">
-        <li><a href="/mytube">Home</a></li>
-        <li><a href="/mytube/private">Private</a></li>
-    </ul>
-</nav>
+    <div class="navbar-wrapper">
+      <div class="container">
+
+        <div class="navbar navbar-inverse navbar-static-top" role="navigation">
+          <div class="container">
+            <div class="navbar-header">
+              <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+              </button>
+              <a class="navbar-brand" href="#">MyTube</a>
+            </div>
+            <div class="navbar-collapse collapse">
+              <ul class="nav navbar-nav">
+                <li><a href="/mytube">Home</a></li>
+                <li><a href="/mytube/private">Private</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
